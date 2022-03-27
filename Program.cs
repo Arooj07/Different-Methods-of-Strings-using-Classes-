d2554b6ac42fe5467461f7cbb75fb25d41fd78ca
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2022-03-27 12:40:52 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRINGMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            strMethodcheck obj = new strMethodcheck();
+            Console.WriteLine("Enter First String:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Entered First String is: " + str1);
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter Second String:");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("Entered Second String is: " + str2);
+            Console.WriteLine("\n");
+            obj.string_method1(str1);
+            obj.string_method2(str1,str2);
+
+
+        }
+    }
+}
---
+++ strMethodcheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRINGMethod
+{
+    internal class strMethodcheck
+    {
+        public void string_method1(string s)
+          {
+            Console.WriteLine("Output of String.Copy() Method");
+            string New1;
+            New1 = string.Copy(s);
+            Console.WriteLine("New String After copying is: {0}", New1);
+            Console.WriteLine("\n");
+        }
+        public void string_method2(string s1, string s2)
+        {
+            Console.WriteLine("Output of String.Compare() Method");
+            int New2;
+            New2 = string.Compare(s1,s2);
+            if (New2 == 0)
+                Console.WriteLine("Both Strings are same: {0}", New2.ToString());
+            else if (New2 > 0)
+                Console.WriteLine("First string is greater than second string: {0}", New2.ToString());
+            else
+                Console.WriteLine("Second String is greater than first string: {0}", New2.ToString());
+        
+
+        }
+    }
+}
